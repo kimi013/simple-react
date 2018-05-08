f279a395da2b6734b434f5c6110a5522fa8fbfbd
liuyang <yliu234@126.com>
2018-05-08 18:13:44 +0800

    react文件夹

--- src/react/set-state-queue.js
+++ src/react/set-state-queue.js
@@ -1,52 +1,72 @@
-import { renderComponent } from '../react-dom/diff'
+import {renderComponent} from '../react-dom/diff'
 
+// 放置stateChange与对应组件的数组
 const setStateQueue = [];
+// 放置有关组件的数组
 const renderQueue = [];
 
-function defer( fn ) {
-    return Promise.resolve().then( fn );
+
+/**
+ * 异步执行
+ * @param fn
+ * @returns {Promise.<TResult>}
+ */
+function defer(fn) {
+    return Promise.resolve().then(fn);
 }
 
-export function enqueueSetState( stateChange, component ) {
 
-    if ( setStateQueue.length === 0 ) {
-        defer( flush );
+/**
+ * 将stateChange于相关组件放入任务队列，异步执行flush
+ * @param stateChange
+ * @param component
+ */
+export function enqueueSetState(stateChange, component) {
+
+    if (setStateQueue.length === 0) {
+        defer(flush);
     }
-    setStateQueue.push( {
+
+    setStateQueue.push({
         stateChange,
         component
-    } );
+    });
 
-    if ( !renderQueue.some( item => item === component ) ) {
-        renderQueue.push( component );
+    if (!renderQueue.some(item => item === component)) {
+        renderQueue.push(component);
     }
 }
 
+
+/**
+ * 清空setStateQueue, 改变各state
+ * 清空renderQueue, 渲染个组件
+ */
 function flush() {
     let item, component;
-    while( item = setStateQueue.shift() ) {
+    while (item = setStateQueue.shift()) {
 
-        const { stateChange, component } = item;
+        const {stateChange, component} = item;
 
         // 如果没有prevState，则将当前的state作为初始的prevState
-        if ( !component.prevState ) {
-            component.prevState = Object.assign( {}, component.state );
+        if (!component.prevState) {
+            component.prevState = Object.assign({}, component.state);
         }
 
         // 如果stateChange是一个方法，也就是setState的第二种形式
-        if ( typeof stateChange === 'function' ) {
-            Object.assign( component.state, stateChange( component.prevState, component.props ) );
+        if (typeof stateChange === 'function') {
+            Object.assign(component.state, stateChange(component.prevState, component.props));
         } else {
             // 如果stateChange是一个对象，则直接合并到setState中
-            Object.assign( component.state, stateChange );
+            Object.assign(component.state, stateChange);
         }
 
         component.prevState = component.state;
 
     }
 
-    while( component = renderQueue.shift() ) {
-        renderComponent( component );
+    while (component = renderQueue.shift()) {
+        renderComponent(component);
     }
 
 }
